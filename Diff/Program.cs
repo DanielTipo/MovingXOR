8ea096f9f08e5d7b6b6c73ae4a78f4d64f1239c9
Dániel Adamkó <danielhun88@hotmail.com>
2016-03-06 20:53:13 +0100

    init

---
+++ Diff/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: diff \"file1\" \"file2\"");
+                return;
+            }
+
+            var file1 = args[0];
+            if (!File.Exists(file1))
+            {
+                Console.Error.WriteLine("File1 not found");
+                return;
+            }
+            var file2 = args[1];
+            if (!File.Exists(file2))
+            {
+                Console.Error.WriteLine("File2 not found");
+                return;
+            }
+
+            int numofdiffs = 0;
+            int length = 0;
+            using (var f1 = new FileStream(file1, FileMode.Open))
+            using (var f2 = new FileStream(file2, FileMode.Open))
+            using (var o = new FileStream(file1 + "_" + file2 + ".diff", FileMode.OpenOrCreate))
+            {
+
+                length = (int)Math.Min(f1.Length, f2.Length);
+                int read1 = 0;
+                int read2 = 0;
+                var r1 = new byte[512];
+                var r2 = new byte[512];
+                while (0 < (read1 = f1.Read(r1, 0, 512))
+                    && 0 < (read2 = f2.Read(r2, 0, 512)))
+                {
+                    var ob = new byte[Math.Min(read1, read2)];
+                    for (int i = 0; i < read1 && i < read2; i++)
+                    {
+                        if (r1[i] == r2[i])
+                        {
+                            ob[i] = (byte)r1[i];
+                            numofdiffs++;
+                        }
+                        else
+                            ob[i] = 0;
+                    }
+                    o.Write(ob, 0, ob.Length);
+                }
+            }
+
+            Console.WriteLine("Difference: {0:N2}%, {1}", ((double)(length - numofdiffs) / length * 100), numofdiffs);
+
+
+        }
+    }
+}
---
+++ MovingXor/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovingXor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: xor2 \"file\" \"code\"");
+                return;
+            }
+            var code = args[1];
+            if (code.Length < 8)
+            {
+                Console.Error.WriteLine("Code is shorter than 8 charaters");
+                return;
+            }
+            var file = args[0];
+            if (!File.Exists(file))
+            {
+                Console.Error.WriteLine("File not exists");
+                return;
+            }
+            var file2 = file + ".xor2";
+            Console.Write("Working.");
+            using (var sr = new FileStream(file, FileMode.Open))
+            using (var sw = new FileStream(file2, FileMode.OpenOrCreate))
+            {
+                var cpos = 0;
+                var rpos = 1;
+                var read = 0;
+                var rbuffer = new byte[512];
+                while (0 < (read = sr.Read(rbuffer, 0, 512)))
+                {
+                    var rnd = new Random(getcode(cpos, rpos, code));
+                    var wbuffer = new byte[read];
+                    var xbuffer = new byte[read];
+                    rnd.NextBytes(xbuffer);
+                    for (int i = 0; i < read; ++i)
+                        wbuffer[i] = (byte)((byte)rbuffer[i] ^ xbuffer[i]);
+                    sw.Write(wbuffer, 0, read);
+                    cpos++;
+                    rpos++;
+                    if (cpos == code.Length) cpos = 0;
+                    if (rpos % 1000 == 0) Console.Write('.');
+                }
+            }
+            Console.Write("Done.\r\n");
+            Console.WriteLine("Output: " + file2);
+        }
+
+        private static int getcode(int cpos, int rpos, string code)
+        {
+            int result = 0;
+            int pos = 0;
+            int rposs = 128 - rpos % 255;
+            while (pos++ < code.Length)
+            {
+                result += (int)Math.Pow(1.4142135, pos) * (int)code[cpos] * rposs;
+                if (cpos >= code.Length - 1) cpos = 0;
+                cpos++;
+            }
+            return result;
+        }
+    }
+}
